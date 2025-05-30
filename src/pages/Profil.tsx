5492eefce471ab11b972332efce300f437098eae
Félicien COURDESSE <felicien.courdesse@francelink.fr>
2025-05-30 11:15:15 +0200

    update lvl max relique

--- src/pages/Profil.tsx
+++ src/pages/Profil.tsx
@@ -48,7 +48,7 @@ export default function Profil() {
 
       // Limite spécifique pour le niveau de relique
       if (key === "Level_Relique") {
-        newValue = Math.min(parsed, 201);
+        newValue = Math.min(parsed, 1000);
       }
 
       return { ...prev, [key]: newValue };
